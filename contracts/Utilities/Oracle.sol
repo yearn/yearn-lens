5fd3e6042f094b69a66cb837b468a3be8b95f907
nymmrx <nymmrx@protonmail.com>
2021-03-13 12:57:25 +0100

    fix(oracle): add WETH routing

--- contracts/Utilities/Oracle.sol
+++ contracts/Utilities/Oracle.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.2;
 
-// import "./Curve.sol";
 import "../../interfaces/Sushiswap/Factory.sol";
 import "../../interfaces/Sushiswap/Router.sol";
 import "../../interfaces/Common/IERC20.sol";
-import "../../../interfaces/Curve/Registry.sol";
+import "../../interfaces/Curve/Registry.sol";
 
 contract Oracle {
     // Factories
@@ -22,20 +23,41 @@ contract Oracle {
     PriceRouter uniswapRouter = PriceRouter(uniswapRouterAddress);
 
     // Constants
-    address usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address USDCAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address WETHAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address ETHAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     function getPriceFromRouter(address token0Address, address token1Address)
         public
         view
         returns (uint256)
     {
-        address[] memory path = new address[](2);
-        path[0] = token0Address;
-        path[1] = token1Address;
+        // Convert ETH address (0xEeee...) to WETH
+        if (token0Address == ETHAddress) {
+            token0Address = WETHAddress;
+        }
+        if (token1Address == ETHAddress) {
+            token1Address = WETHAddress;
+        }
+
+        address[] memory path;
+        if (token0Address == WETHAddress || token1Address == WETHAddress) {
+            // If WETH is already in the path, create a simple path ...
+            path = new address[](2);
+            path[0] = token0Address;
+            path[1] = token1Address;
+        } else {
+            // ... otherwise add WETH in the middle of the path.
+            path = new address[](3);
+            path[0] = token0Address;
+            path[1] = WETHAddress;
+            path[2] = token1Address;
+        }
+
         IERC20 token0 = IERC20(token0Address);
         uint256 amountIn = 10**uint256(token0.decimals());
         uint256[] memory amountsOut;
+
         try uniswapRouter.getAmountsOut(amountIn, path) returns (
             uint256[] memory _amountsOut
         ) {
@@ -53,7 +75,7 @@ contract Oracle {
         view
         returns (uint256)
     {
-        return getPriceFromRouter(tokenAddress, usdcAddress);
+        return getPriceFromRouter(tokenAddress, USDCAddress);
     }
 
     function getPriceUsdc(address tokenAddress) public view returns (uint256) {
@@ -65,9 +87,9 @@ contract Oracle {
     }
 
     // Curve
-    address curveRegistryAddress = 0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c;
-    address zeroAddress = 0x0000000000000000000000000000000000000000;
-    CurveRegistry curveRegistry = CurveRegistry(curveRegistryAddress);
+    address CurveRegistryAddress = 0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c;
+    address NULLAddress = 0x0000000000000000000000000000000000000000;
+    CurveRegistry curveRegistry = CurveRegistry(CurveRegistryAddress);
 
     function getCurvePriceUsdc(address curveLpTokenAddress)
         public
@@ -76,7 +98,7 @@ contract Oracle {
     {
         uint256 basePrice = getBasePrice(curveLpTokenAddress);
         uint256 virtualPrice = getVirtualPrice(curveLpTokenAddress);
-        IERC20 usdc = IERC20(usdcAddress);
+        IERC20 usdc = IERC20(USDCAddress);
         uint256 decimals = usdc.decimals();
         uint256 decimalsAdjustment = 18 - decimals;
         uint256 price =
@@ -110,7 +132,7 @@ contract Oracle {
     function isCurveLpToken(address tokenAddress) public view returns (bool) {
         address poolAddress =
             curveRegistry.get_pool_from_lp_token(tokenAddress);
-        bool tokenHasCurvePool = poolAddress != zeroAddress;
+        bool tokenHasCurvePool = poolAddress != NULLAddress;
         return tokenHasCurvePool;
     }
 
