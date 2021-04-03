dbbcb942b7d0f8d6c6557708ea8dbff547d13b10
x48 <x48114@protonmail.com>
2021-04-02 23:06:00 -0500

    refactor: update lens methods

--- contracts/Lens.sol
+++ contracts/Lens.sol
@@ -3,98 +3,160 @@
 pragma solidity ^0.8.2;
 pragma experimental ABIEncoderV2;
 
-import "../interfaces/Yearn/RegistryAdapter.sol";
 import "./Utilities/Manageable.sol";
 
-// TODO: Implement authentication
+// Interfaces and helpers
+interface IAdapter {
+    struct Asset {
+        string name;
+        address id;
+        string version;
+    }
+
+    struct Position {
+        address assetId;
+        uint256 depositedBalance;
+        uint256 tokenBalance;
+        uint256 tokenAllowance;
+    }
+
+    struct AdapterInfo {
+        address id;
+        string typeId;
+        string categoryId;
+        string subcategoryId;
+    }
+
+    function adapterInfo() external view returns (AdapterInfo memory);
+
+    function assets() external view returns (Asset[] memory);
+
+    function positionsOf(address account)
+        external
+        view
+        returns (Position[] memory);
+
+    function asset() external view returns (Asset memory);
+
+    function assetsLength() external view returns (uint256);
+
+    function assetsAddresses() external view returns (address[] memory);
+}
+
+// Main contract
 contract Lens is Manageable {
-    mapping(uint256 => address) private registries;
-    uint256 public numRegistries;
-    mapping(address => uint256) private isRegistered;
+    uint256 public adaptersLength;
+    mapping(address => uint256) private adapterIdxByAddress;
+    mapping(uint256 => address) private adapterAddressByIdx;
 
     constructor(address _managementListAddress)
         Manageable(_managementListAddress)
     {}
 
-    function addAdapter(address registryAddress) public onlyManagers {
-        if (isRegistered[registryAddress] == 0) {
-            numRegistries += 1;
-            registries[numRegistries] = registryAddress;
-            isRegistered[registryAddress] = numRegistries;
+    function addAdapter(address adapterAddress) public onlyManagers {
+        bool adapterDoesntExist = adapterIdxByAddress[adapterAddress] == 0;
+        if (adapterDoesntExist) {
+            adaptersLength += 1;
+            adapterAddressByIdx[adaptersLength] = adapterAddress;
+            adapterIdxByAddress[adapterAddress] = adaptersLength;
         }
     }
 
-    function addAdapters(address[] memory registryAddresses)
+    function addAdapters(address[] memory adapterAddresses)
         public
         onlyManagers
     {
-        for (uint256 i = 0; i < registryAddresses.length; i++) {
-            address registryAddress = registryAddresses[i];
-            addAdapter(registryAddress);
+        for (
+            uint256 adapterIdx = 0;
+            adapterIdx < adapterAddresses.length;
+            adapterIdx++
+        ) {
+            address adapterAddress = adapterAddresses[adapterIdx];
+            addAdapter(adapterAddress);
         }
     }
 
     // TODO: Implement
-    // function replaceRegistry(address oldAddress, address newAddress) public {}
+    // function replaceAdapter(address oldAddress, address newAddress) public {}
 
-    function removeAdapter(address registryAddress) public onlyManagers {
-        if (isRegistered[registryAddress] != 0) {
-            uint256 registryIndex = isRegistered[registryAddress];
-            delete registries[registryIndex];
-            delete isRegistered[registryAddress];
-            numRegistries -= 1;
+    function removeAdapter(address adapterAddress) external onlyManagers {
+        bool adapterExists = adapterIdxByAddress[adapterAddress] != 0;
+        if (adapterExists) {
+            uint256 adapterIndex = adapterIdxByAddress[adapterAddress];
+            delete adapterAddressByIdx[adapterIndex];
+            delete adapterIdxByAddress[adapterAddress];
+            adaptersLength -= 1;
         }
     }
 
-    function getAdapters() external view returns (address[] memory) {
-        address[] memory registryList = new address[](numRegistries);
-        for (uint256 i = 0; i < numRegistries; i++) {
-            address registryAddress = registries[i + 1];
-            registryList[i] = registryAddress;
-        }
-        return registryList;
-    }
-
-    function getAssetsFromAdapter(RegistryAdapter registryAdapterAddress)
+    function adaptersInfo()
         external
         view
-        returns (RegistryAdapter.Asset[] memory)
+        returns (IAdapter.AdapterInfo[] memory)
     {
-        RegistryAdapter registryAdapter =
-            RegistryAdapter(registryAdapterAddress);
-        return registryAdapter.getAssets();
-    }
-
-    function getAssetsLength() public view returns (uint256) {
-        uint256 assetsLength;
+        IAdapter.AdapterInfo[] memory _adaptersInfo =
+            new IAdapter.AdapterInfo[](adaptersLength);
         for (
-            uint256 registryIdx = 0;
-            registryIdx < numRegistries;
-            registryIdx++
+            uint256 adapterIdx = 0;
+            adapterIdx < adaptersLength;
+            adapterIdx++
         ) {
-            address registryAdapterAddress = registries[registryIdx + 1];
-            RegistryAdapter registryAdapter =
-                RegistryAdapter(registryAdapterAddress);
-            assetsLength += registryAdapter.getAssetsLength();
+            address adapterAddress = adapterAddressByIdx[adapterIdx + 1];
+            IAdapter.AdapterInfo memory adapterInfo =
+                IAdapter(adapterAddress).adapterInfo();
+            _adaptersInfo[adapterIdx] = adapterInfo;
         }
-        return assetsLength;
+        return _adaptersInfo;
     }
 
-    // TODO: Implement array concat.. memcpy..?
-    function getAssetsAddresses() public view returns (address[] memory) {
-        uint256 assetsLength = getAssetsLength();
+    function adapters() external view returns (address[] memory) {
+        address[] memory adapterList = new address[](adaptersLength);
+        for (
+            uint256 adapterIdx = 0;
+            adapterIdx < adaptersLength;
+            adapterIdx++
+        ) {
+            address adapterAddress = adapterAddressByIdx[adapterIdx + 1];
+            adapterList[adapterIdx] = adapterAddress;
+        }
+        return adapterList;
+    }
+
+    function assetsFromAdapter(IAdapter adapterAddress)
+        external
+        view
+        returns (IAdapter.Asset[] memory)
+    {
+        IAdapter adapter = IAdapter(adapterAddress);
+        return adapter.assets();
+    }
+
+    function assetsLength() public view returns (uint256) {
+        uint256 _assetsLength;
+        for (
+            uint256 adapterIdx = 0;
+            adapterIdx < adaptersLength;
+            adapterIdx++
+        ) {
+            address adapterAddress = adapterAddressByIdx[adapterIdx + 1];
+            IAdapter adapter = IAdapter(adapterAddress);
+            _assetsLength += adapter.assetsLength();
+        }
+        return _assetsLength;
+    }
+
+    function assetsAddresses() public view returns (address[] memory) {
+        uint256 assetsLength = assetsLength();
         address[] memory assetsAddresses = new address[](assetsLength);
         uint256 assetIdx;
         for (
-            uint256 registryIdx = 0;
-            registryIdx < numRegistries;
-            registryIdx++
+            uint256 adapterIdx = 0;
+            adapterIdx < adaptersLength;
+            adapterIdx++
         ) {
-            address registryAdapterAddress = registries[registryIdx + 1];
-            RegistryAdapter registryAdapter =
-                RegistryAdapter(registryAdapterAddress);
-            address[] memory assetAddresses =
-                registryAdapter.getAssetsAddresses();
+            address adapterAddress = adapterAddressByIdx[adapterIdx + 1];
+            IAdapter adapter = IAdapter(adapterAddress);
+            address[] memory assetAddresses = adapter.assetsAddresses();
             for (
                 uint256 registryAssetIdx = 0;
                 registryAssetIdx < assetAddresses.length;
@@ -107,70 +169,58 @@ contract Lens is Manageable {
         return assetsAddresses;
     }
 
-    // TODO: Is there a better way to do this?
-    function getAssets()
-        external
-        view
-        returns (RegistryAdapter.Asset[] memory)
-    {
-        uint256 assetsLength = getAssetsLength();
-        RegistryAdapter.Asset[] memory assetsList =
-            new RegistryAdapter.Asset[](assetsLength);
+    function assets() external view returns (IAdapter.Asset[] memory) {
+        uint256 _assetsLength = assetsLength();
+        IAdapter.Asset[] memory assetsList =
+            new IAdapter.Asset[](_assetsLength);
         uint256 assetIdx;
         for (
-            uint256 registryIdx = 0;
-            registryIdx < numRegistries;
-            registryIdx++
+            uint256 adapterIdx = 0;
+            adapterIdx < adaptersLength;
+            adapterIdx++
         ) {
-            address registryAdapterAddress = registries[registryIdx + 1];
-            RegistryAdapter registryAdapter =
-                RegistryAdapter(registryAdapterAddress);
-            RegistryAdapter.Asset[] memory registryAssets =
-                registryAdapter.getAssets();
-
-            // TODO: Can this be moved into a separate function? Dislike nested for loops..
+            address adapterAddress = adapterAddressByIdx[adapterIdx + 1];
+            IAdapter adapter = IAdapter(adapterAddress);
+            IAdapter.Asset[] memory adapterAssets = adapter.assets();
             for (
-                uint256 registryAssetIdx = 0;
-                registryAssetIdx < registryAssets.length;
-                registryAssetIdx++
+                uint256 adapterAssetIdx = 0;
+                adapterAssetIdx < adapterAssets.length;
+                adapterAssetIdx++
             ) {
-                assetsList[assetIdx] = registryAssets[registryAssetIdx];
+                assetsList[assetIdx] = adapterAssets[adapterAssetIdx];
                 assetIdx++;
             }
         }
         return assetsList;
     }
 
-    function getPositionsFromAdapter(
-        address account,
-        RegistryAdapter registryAdapterAddress
-    ) external view returns (RegistryAdapter.Position[] memory) {
-        RegistryAdapter registryAdapter =
-            RegistryAdapter(registryAdapterAddress);
-        return registryAdapter.getPositionsOf(account);
-    }
-
-    // TODO: Refactor..
-    function getPositionsOf(address account)
+    function positionsFromAdapter(address account, IAdapter adapterAddress)
         external
         view
-        returns (RegistryAdapter.Position[] memory)
+        returns (IAdapter.Position[] memory)
     {
-        uint256 assetsLength = getAssetsLength();
-        RegistryAdapter.Position[] memory positionsList =
-            new RegistryAdapter.Position[](assetsLength);
+        IAdapter adapter = IAdapter(adapterAddress);
+        return adapter.positionsOf(account);
+    }
+
+    function positionsOf(address account)
+        external
+        view
+        returns (IAdapter.Position[] memory)
+    {
+        uint256 _assetsLength = assetsLength();
+        IAdapter.Position[] memory positionsList =
+            new IAdapter.Position[](_assetsLength);
         uint256 assetIdx;
         for (
-            uint256 registryIdx = 0;
-            registryIdx < numRegistries;
-            registryIdx++
+            uint256 adapterIdx = 0;
+            adapterIdx < adaptersLength;
+            adapterIdx++
         ) {
-            address registryAdapterAddress = registries[registryIdx + 1];
-            RegistryAdapter registryAdapter =
-                RegistryAdapter(registryAdapterAddress);
-            RegistryAdapter.Position[] memory adapterPositions =
-                registryAdapter.getPositionsOf(account);
-            // TODO: Can this be moved into a separate function? Dislike nested for loops..
+            address adapterAddress = adapterAddressByIdx[adapterIdx + 1];
+            IAdapter adapter = IAdapter(adapterAddress);
+            IAdapter.Position[] memory adapterPositions =
+                adapter.positionsOf(account);
             for (
                 uint256 registryAssetIdx = 0;
                 registryAssetIdx < adapterPositions.length;
