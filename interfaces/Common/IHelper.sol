3bcf1503bb53ce7b118bf3dd3cfbbbb6a5fb1116
x48 <x48114@protonmail.com>
2021-04-12 22:42:43 -0500

    refactor: add helper interface

--- interfaces/Common/IHelper.sol
+++ interfaces/Common/IHelper.sol
@@ -3,11 +3,13 @@
 pragma solidity >=0.8.2;
 
 interface IHelper {
+    // Addresses merge helper
     function mergeAddresses(address[][] memory addressesSets)
         external
         view
         returns (address[] memory);
 
+    // Strategies helper
     function assetStrategiesDelegatedBalance(address)
         external
         view
@@ -18,4 +20,18 @@ interface IHelper {
     function assetStrategiesLength(address) external view returns (uint256);
 
     function assetsStrategiesLength() external view returns (uint256);
+
+    // Allowances helper
+    struct Allowance {
+        address owner;
+        address spender;
+        uint256 amount;
+        address token;
+    }
+
+    function allowances(
+        address ownerAddress,
+        address[] memory tokensAddresses,
+        address[] memory spenderAddresses
+    ) external view returns (Allowance[] memory);
 }
