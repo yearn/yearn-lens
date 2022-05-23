f44961144b3facedca1cf8d13557e5f89ac1750b
jstash <19509999+jstashh@users.noreply.github.com>
2022-05-23 17:08:31 +0100

    feat: document functions

--- contracts/Utilities/PropertiesAggregator.sol
+++ contracts/Utilities/PropertiesAggregator.sol
@@ -2,10 +2,16 @@
 pragma solidity 0.8.11;
 
 contract PropertiesAggregator {
+    /**
+     * @notice Fetches a single property from the target contract, assuming no arguments are used for the property
+     * @param target The contract to perform the query on
+     * @param name The name of the property to query
+     * @return The raw data of the output of the call, not yet decoded
+     */
     function getProperty(address target, string calldata name)
         public
         view
-        returns (bytes memory res)
+        returns (bytes memory)
     {
         string memory methodSignature = string(abi.encodePacked(name, "()"));
         (bool success, bytes memory result) = target.staticcall(
@@ -16,6 +22,12 @@ contract PropertiesAggregator {
         }
     }
 
+    /**
+     * @notice Simultaneously fetches multiple properties from the target contract, assuming no arguments are used for each property
+     * @param target The contract to perform the query on
+     * @param names The names of the properties to query
+     * @return An array of the raw data of the output of each property query, not yet decoded
+     */
     function getProperties(address target, string[] calldata names)
         external
         view
